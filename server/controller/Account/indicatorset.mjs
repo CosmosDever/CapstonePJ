fc6d3e974eae3a5c5c6867a4b3442f00f2a023f5
CosmosDever <aprommhgot@gmail.com>
2024-02-28 20:20:04 +0700

    Update indicatorset.mjs

--- server/controller/Account/indicatorset.mjs
+++ server/controller/Account/indicatorset.mjs
@@ -1,16 +1,20 @@
 export const setindicator = async (req, res) => {
   try {
-    const indicator = {
-      indicator1: req.body.indicator1,
-      indicator2: req.body.indicator2,
-      indicator3: req.body.indicator3,
-      indicator4: req.body.indicator4,
-      indicator5: req.body.indicator5,
-      indicator6: req.body.indicator6,
-    };
+    const indicator = req.body;
     res.send(indicator);
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: "Internal Server Error" });
   }
 };
+
+// {
+//       indicator1: "",
+//       indicator2: "",
+//       indicator3: "",
+//       indicator4: "",
+//       indicator5: "",
+//       indicator6: "",
+//       timepre : "",
+//       amount: "",
+// }
