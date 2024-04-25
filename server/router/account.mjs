edb5aef5a4a7693f127e4fcbe55f097474cb8968
oattao123 <72804848+oattao123@users.noreply.github.com>
2024-04-25 18:18:04 +0700

    .

--- server/router/account.mjs
+++ server/router/account.mjs
@@ -5,11 +5,13 @@ import { setindicator } from "../controller/Account/indicatorset.mjs";
 import { signup } from "../controller/Account/signup.mjs";
 import { signin } from "../controller/Account/signin.mjs";
 import { signout } from "../controller/Account/signout.mjs";
+import { getindicator } from "../controller/Account/getindicator.mjs";
 const router = express.Router();
 
 router.post("/setapi", api);
 router.get("/getBalance", fetchBalances);
 router.post("/setindicator", setindicator);
+router.post("/getindicator", getindicator);
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
