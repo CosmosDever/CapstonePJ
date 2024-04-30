c660992980b1c7d9440a13d9779a91ec0a0c20fc
CosmosDever <aprommhgot@gmail.com>
2024-04-30 22:25:59 +0700

    get24hStatististics

---
+++ server/controller/BuyandSell/get24hStatististics.mjs
@@ -0,0 +1,45 @@
+import { MainClient } from "binance";
+import { LocalStorage } from "node-localstorage";
+const localStorage = new LocalStorage("./scratch");
+const formatVolumeUSDT = (volume) => {
+  const billion = 1000000000;
+  const million = 1000000;
+  if (volume >= billion) {
+    return (volume / billion).toFixed(2) + "B";
+  } else if (volume >= million) {
+    return (volume / million).toFixed(2) + "M";
+  } else {
+    return volume.toFixed(2);
+  }
+};
+export const get24hStatististics = async (req, res) => {
+  try {
+    const client = new MainClient({
+      api_key: localStorage.getItem("API_KEY"),
+      api_secret: localStorage.getItem("API_SECRET"),
+    });
+    const coinStatististics = await client.get24hrChangeStatististics({
+      symbol: "BTCUSDT",
+    });
+    const coinStatisticsArray = Array.isArray(coinStatististics)
+      ? coinStatististics
+      : [coinStatististics];
+    const coinStatististicsres = coinStatisticsArray.map((res) => {
+      return {
+        symbol: res.symbol,
+        openPrice: String(parseFloat(res.openPrice).toFixed(2)),
+        priceChangePercent: String(
+          parseFloat(res.priceChangePercent).toFixed(2)
+        ),
+        highPrice: String(parseFloat(res.highPrice).toFixed(2)),
+        lowPrice: String(parseFloat(res.lowPrice).toFixed(2)),
+        volumeBTC: String(parseFloat(res.volume).toFixed(2)),
+        volumeUSDT: String(formatVolumeUSDT(res.volume * res.openPrice)),
+      };
+    });
+    res.send(coinStatististicsres);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+};
--- server/router/buyandsell.mjs
+++ server/router/buyandsell.mjs
@@ -1,7 +1,9 @@
 import express from "express";
 import { buysellorder } from "../controller/BuyandSell/buysell.mjs";
 import { getorder } from "../controller/BuyandSell/getorder.mjs";
+import { get24hStatististics } from "../controller/BuyandSell/get24hStatististics.mjs";
 const router = express.Router();
 router.post("/BuySellOrder", buysellorder);
 router.get("/GetOrder", getorder);
+router.get("/Get24hStatististics", get24hStatististics);
 export default router;
