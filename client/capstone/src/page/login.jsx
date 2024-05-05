1ae8129ce4197acebfec7bed6f03c249c1509a76
Thtr <115204228+Thtr1712@users.noreply.github.com>
2024-05-06 01:08:40 +0700

    css with (dummy)function

--- client/capstone/src/page/login.jsx
+++ client/capstone/src/page/login.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import '../css/login.css'
 
 const Login = () =>{
+    const [data, setData] = useState({username: '', pw: '',});
+
+    const handleChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value });
+    };
+
+
+    //อันนี้ใส่ไว้เป็นdummyก่อน
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:3009/', {
+                username: data.username,
+                pw: data.pw,
+            });
+            if (response.data.status === "success") {
+                alert(response.data.success);
+                navigate('/home');
+            }
+            else {
+                console.log('Login failed. Error:', response.data.error);
+                alert('Incorrect username or password. Please try again.');
+            }
+        } catch (error) {
+            console.error('Authentication error:', error);
+            alert('Authentication failed. Please try again.');
+        }
+        setFormData({ username: '', pw: '' });
+    };
+    
+
 
     return(
         <div className="login">
             <div className="container">
                 <h1>Login</h1>
+
                 <div className="input-fields">
-                    <form action="">
+                    <form action="" onSubmit={handleSubmit}>
 
                         <label>
                             Username<br />
-                            <input type="text" id="username" Username="name" />
+                            <input type="text" id="username" Username="name" onChange={handleChange} required />
                         </label><br />
 
                         <label>
                             Password<br />
-                            <input type="password" id="password" Username="name" />
+                            <input type="password" id="password" Username="name" onChange={handleChange} required />
                         </label><br />
 
-                        <input type="submit" />
+                        <input type="submit" value="Log in" />
                     </form>
                     <p>Don't have account? <a href="/">Create account</a></p>  
                     
@@ -31,4 +63,4 @@ const Login = () =>{
 }
 
 
-export default Login
+export default Login;
