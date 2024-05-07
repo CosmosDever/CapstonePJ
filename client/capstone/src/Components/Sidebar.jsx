34a2c321010cea90fbf20dabaa38a95bcc5b2258
Ptrchn_deaw <deawandnathai@gmail.com>
2024-05-07 19:44:24 +0700

    .

--- client/capstone/src/Components/Sidebar.jsx
+++ client/capstone/src/Components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Sidebar.css"
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
 
 function sidebar() {
   const [username,setUsername] = useState('') // รับค่า Username จาก back
@@ -9,11 +10,14 @@ function sidebar() {
     <div>
       <div className='sdbar_bg'></div>
       <div className='sdbar'>
-        <div className='text_name'> Poomthai Promgote 
-          <div className='acc_balance'> Account Balance</div>
-          <div className='dollar_balance'>$100000</div>
-        </div>
+        <div className='text_name'> Poomthai Promgote </div>
+        <div className='acc_balance'> Account Balance</div>
+        <div className='dollar_balance'>$100000</div>
+        <div className='chart'>Chart</div>
+        <div className='trade'> Trading </div>
+        <div className='setting'> Setting </div>
       </div>
+      <div></div>
     </div>
   )
 }
--- client/capstone/src/main.jsx
+++ client/capstone/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Route } from 'react-router-dom'
+import Chart from './App/Chart'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
