88e29b21bf31053ebabe916cb8a7594efcc8a082
Thtr <115204228+Thtr1712@users.noreply.github.com>
2024-05-08 21:51:02 +0700

    Signup page with function

--- client/capstone/src/page/signup.jsx
+++ client/capstone/src/page/signup.jsx
@@ -1,35 +1,45 @@
-import React, { useState,useEffect } from "react";
+import React, { useState} from "react";
 import '../css/signup.css'
+import axios from 'axios'
 
 const Register = () =>{
-    const [data, setData] = useState({username: '', pw: '',});
+    const [data, setData] = useState({username: '',email: '', password: '',Cpassword:''});
 
     const handleChange = (e) => {
         setData({ ...data, [e.target.name]: e.target.value });
     };
 
 
-
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:3009/', {
-                username: data.username,
-                pw: data.pw,
-            });
-            if (response.data.status === "success") {
-                alert(response.data.success);
-                navigate('/home');
-            }
-            else {
-                console.log('Login failed. Error:', response.data.error);
-                alert('Incorrect username or password. Please try again.');
-            }
-        } catch (error) {
-            console.error('Authentication error:', error);
-            alert('Authentication failed. Please try again.');
+
+        if (data.password !== data.Cpassword) {
+            alert('Password and confirm password do not match. Please try again');
+            return;
         }
-        setFormData({ username: '', pw: '' });
+
+        try {
+
+            const response = await axios.post('http://localhost:3605/Account/signup', {
+                username: data.username,
+                email: data.email,
+                password: data.password,
+            })
+
+
+            if(response.data.status === "error"){
+                alert(response.data.error)
+            }
+            else{
+                alert(response.data.success)
+                navigate('/login')
+            }
+
+        } catch (error) {
+            console.error('Sign Up error', error);
+            alert('Sign Up failed. Please try again.');
+        }
+        setData({ username: '',email: '', password: '',Cpassword:'' });
     };
     
 
@@ -44,25 +54,25 @@ const Register = () =>{
 
                         <label>
                             Username<br />
-                            <input type="text" id="username"  className="Typeinput" onChange={handleChange} required />
+                            <input type="text"  name="username"  className="Typeinput" onChange={handleChange} required />
                         </label><br />
 
                         <label>
                             Email<br />
-                            <input type="Email" id="email"className="Typeinput" onChange={handleChange} required />
+                            <input type="email" name="email"className="Typeinput" onChange={handleChange} required />
                         </label><br />
 
                         <label>
                             Password<br />
-                            <input type="password" id="password" className="Typeinput" onChange={handleChange} required />
+                            <input type="password" name="password" className="Typeinput" onChange={handleChange} required />
                         </label><br />
 
                         <label>
                             Confirm Password<br />
-                            <input type="password" id="password"  className="Typeinput" onChange={handleChange} required />
+                            <input type="password" name="Cpassword"  className="Typeinput" onChange={handleChange} required />
                         </label><br />
 
-                        <input type="submit" value="Log in" />
+                        <input type="submit" value="Create account" />
                     </form>
                     <p>I already have account <a href="/">Sign in</a></p>  
                     
