b28e419565f798b359ef8caa6230860bc9e3adc5
CosmosDever <aprommhgot@gmail.com>
2024-05-22 12:55:54 +0700

    not ok

--- client/capstone/src/App.jsx
+++ client/capstone/src/App.jsx
@@ -1,36 +1,31 @@
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signin from "./page/login";
-import Signup from "./page/signup";
-import Changepasswordemail from "./page/cpw_email";
-import Changepasswordotp from "./page/cpw_verifyOTP";
-import ChangepasswordNewpassword from "./page/cpw_newpw";
-import Trading from "./page/trading";
+import Signin from "./Page/signin";
+import Signup from "./Page/signup";
+import Changepasswordemail from "./Page/cpw_email";
+import Changepasswordotp from "./Page/cpw_verifyOTP";
+import ChangepasswordNewpassword from "./Page/cpw_newpw";
+import Trading from "./Page/Trading";
 import "./App.css";
-import Chart from "./page/chart";
-import Setting from "./page/setting";
+import Chart from "./Page/Chart";
+import Setting from "./Page/setting";
 import "./index.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Chart />} />
-        <Route path="/Trading" element={<Trading />} />
-        <Route path="/Setting" element={<Setting />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/changepassword" element={<Changepasswordemail />} />
-        <Route
-          path="/changepassword/verifyOTP"
-          element={<Changepasswordotp />}
-        />
-        <Route
-          path="/changepassword/newpassword"
-          element={<ChangepasswordNewpassword />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Chart />} />
+      <Route path="/Trading" element={<Trading />} />
+      <Route path="/Setting" element={<Setting />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/changepassword" element={<Changepasswordemail />} />
+      <Route path="/changepassword/verifyOTP" element={<Changepasswordotp />} />
+      <Route
+        path="/changepassword/newpassword"
+        element={<ChangepasswordNewpassword />}
+      />
+    </Routes>
   );
 }
 
